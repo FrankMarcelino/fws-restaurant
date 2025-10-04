68acd475a11c065438bfb9ab79bb79ae9ba1514b
frankMarcelino <frank.silva.ti@gmail.com>
2025-10-04 16:44:39 -0300

    feat: ensure cart product quantity can be decreased

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -1,10 +1,11 @@
 import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
+import { useContext } from "react";
 
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/helpers/format-currency";
 
-import { CartProduct } from "../contexts/cart";
+import { CartContext, CartProduct } from "../contexts/cart";
 
 interface CartItemProps {
   product: CartProduct;
@@ -12,6 +13,7 @@ interface CartItemProps {
 
 
 const CartProductItem = ({product}: CartItemProps) => {
+  const {decreaseProductQuantity} = useContext(CartContext)
   return ( 
     <div className="flex items-center justify-between">
 
@@ -25,11 +27,11 @@ const CartProductItem = ({product}: CartItemProps) => {
           <p className="text-sm font-semibold">{formatCurrency(product.price)}</p>
           {/* QUANTIDADE */}
           <div className="flex items-center gap-1">
-            <Button variant="outline" className="h-7 w-7 rounded-xl" >
+            <Button variant="outline" className="h-7 w-7 rounded-xl" onClick={() => decreaseProductQuantity(product.id)}>
               <ChevronLeftIcon />
             </Button>
               <p className="w-7 text-xs">{product.quantity}</p>
-            <Button variant="destructive"  className="h-7 w-7 rounded-xl" >
+            <Button variant="destructive"  className="h-7 w-7 rounded-xl" onClick={() => decreaseProductQuantity(product.id)}>
               <ChevronRightIcon />
             </Button>
           </div>
