a57f4ff88cef9ede9297172d2b1f035c2d82d8f6
frankMarcelino <frank.silva.ti@gmail.com>
2025-10-04 13:29:49 -0300

    feat: esure product can be added more than once to cart

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -30,11 +30,28 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
     setIsOpen((prev) => !prev);
   };
   const addProduct = (product: CartProduct) => {
-    setProducts((prev) => 
-    [...prev, product]
+  const productAlreadyOnTheCart = products.some(
+    (prevProduct) => prevProduct.id === product.id,
   );
-  };
-  return (
+
+  if (!productAlreadyOnTheCart) {
+    return setProducts((prev) => [...prev, product]);
+  }
+
+  setProducts((prevProducts) => {
+    return prevProducts.map((prevProduct) => {
+      if (prevProduct.id === product.id) {
+        return {
+          ...prevProduct,
+          quantity: prevProduct.quantity + product.quantity,
+        };
+      }
+      return prevProduct;
+    });
+  });
+};
+
+return (
     <CartContext.Provider
       value={{
         isOpen: isOpen,
